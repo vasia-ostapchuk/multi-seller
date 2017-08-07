27159aa9cee3f8330f1e4553d4db722ded78364f
Vasyl Ostapchuk <ostapchyk.vasia@gmail.com>
2017-08-07 17:13:18 +0200

    Module configuration

--- multiseller.php
+++ multiseller.php
@@ -4,9 +4,24 @@
 class multiseller extends Module
 {
 	function __construct()
-	{
-		$this->name = 'multiseller';
-		$this->tab = 'administation';
-		$this->displayName = $this->l('Multi seller');
-	}
+    {
+        $this->name = 'multiseller';
+        $this->tab = 'administration';
+        $this->author = 'SoftSprint.net';
+        $this->module_key = '44873542187effe440a9606087efd6ee';
+        $this->version = '1.0.0.0';
+        parent::__construct();
+        $this->displayName = $this->l('Multi seller');
+        $this->description = $this->l('');
+    }
+
+    public function inconsistency($return)
+    {
+        return true;
+    }
+
+    public function getContent()
+    {
+
+    }
 }
