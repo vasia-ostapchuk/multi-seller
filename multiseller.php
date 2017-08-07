978a40f7d0a2cb2b042e40035001da998708ea71
Vasyl Ostapchuk <ostapchyk.vasia@gmail.com>
2017-08-07 16:48:31 +0200

    constructor

---
+++ multiseller.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class multiseller extends Module
+{
+	function __construct()
+	{
+		$this->name = 'multiseller';
+		$this->tab = 'administation';
+		$this->displayName = $this->l('Multi seller');
+	}
+}
